710b7cae6d054bdf7fd348fffe2ee992e2141fa7
tylcos <tylcos@users.noreply.github.com>
2017-10-22 14:54:41 -0400

    Initial commit

---
+++ InvalidCharException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Turtle_Tokenizer
+{
+    [Serializable]
+    internal class InvalidCharException : Exception
+    {
+        public InvalidCharException()
+        {
+        }
+
+        public InvalidCharException(string message) : base(message)
+        {
+        }
+
+        public InvalidCharException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidCharException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
