690ba6cdd9d8aa8086f9dfdd2f2737827cde1bec
tylcos <tylcos@users.noreply.github.com>
2017-10-22 14:54:41 -0400

    Initial commit

---
+++ Lexer.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Turtle_Tokenizer
+{
+    public class Lexeme
+    {
+        public readonly string Text;
+        public readonly Lexer.CharType Type;
+
+        public Lexeme() { }
+
+        public Lexeme(string _text, Lexer.CharType _type)
+        {
+            Text = _text;
+            Type = _type;
+        }
+
+        public static Lexeme GetNextLexeme()
+        {
+            StringBuilder text = new StringBuilder(32);
+            Lexer.CharType type;
+
+            if (Program.chars.Count == 1)
+                return new Lexeme(GetValue().ToString(), GetType(true));
+
+            do
+            {
+                type = GetType();
+                text.Append(GetValue(true));
+            }
+            while (Program.chars.Any() && type == GetType() && type != Lexer.CharType.Accessor);
+
+            Lexeme returnLexeme = new Lexeme(text.ToString(), type);
+            return type == Lexer.CharType.Separator ? GetNextLexeme() : returnLexeme;
+
+            char GetValue(bool removeChar = false)
+            {
+                char c = Program.chars.First();
+
+                if (removeChar)
+                    Program.chars.RemoveAt(0);
+
+                return c;
+            }
+
+            Lexer.CharType GetType(bool removeChar = false)
+            {
+                Lexer.CharType _type = Lexer.GetType(GetValue());
+
+                if (removeChar)
+                    Program.chars.RemoveAt(0);
+
+                return _type;
+            }
+        }
+    }
+
+    public class Lexer
+    {
+        public const string Operators = "=+-*/&|!";
+        public const string Separators = ", ";
+        public const string Accessors = "{}()[]";
+
+        public enum CharType
+        {
+            Operator,
+            Literal,
+            Separator,
+            Accessor
+        }
+
+        public static CharType GetType(char c)
+        {
+            if (char.IsLetterOrDigit(c) || c == '.')
+                return CharType.Literal;
+
+            if (Operators.Contains(c))
+                return CharType.Operator;
+
+            if (Accessors.Contains(c))
+                return CharType.Accessor;
+
+            if (Separators.Contains(c))
+                return CharType.Separator;
+
+            throw new InvalidCharException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Turtle_Tokenizer
+{
+    class Program
+    {
+        static Dictionary<string, Variable> variables = new Dictionary<string, Variable>();
+        static Dictionary<string, Token[]> functions = new Dictionary<string, Token[]>();
+
+        static List<Token> tokens = new List<Token>();
+
+        public static List<char> chars;
+
+        static readonly string TEXTPATH = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\a.txt";
+
+        public static void Main()
+        {
+            string lines = File.ReadAllText(TEXTPATH);
+            chars = Regex.Replace(Regex.Replace(Regex.Replace(lines
+                , @"\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*\/+|\t|\/\/.*", "") // Removes comments, multiline comments, and tabs
+                , @"\n|\r", " ")                                                // Replaces new lines
+                , @"\s+", " ")                                                  // Removes excess whitespace
+                .Trim().ToCharArray().ToList();
+
+            List<Lexeme> lexemes = new List<Lexeme>();
+
+            while (chars.Any())
+            {
+                lexemes.Add(Lexeme.GetNextLexeme());
+                Console.WriteLine(lexemes.Last().Text + string.Join("", Enumerable.Repeat(' ', 10 - lexemes.Last().Text.Length)) + lexemes.Last().Type);
+            }
+
+            Console.Read();
+        }
+    }
+
+    public class Evaluator
+    {
+        Lexeme[] Lexemes { get; set; }
+        Token[]  Tokens  { get; set; }
+
+        public Evaluator(Lexeme[] _lexemes)
+        {
+            Lexemes = _lexemes;
+        }
+    }
+
+    public class Variable
+    {
+        public readonly Type Type;
+        public readonly object Value;
+
+        public Variable(Type _type, object _value)
+        {
+            Type = _type;
+            Value = _value;
+        }
+
+        public T GetValue<T>()
+        {
+            return (T)Value;
+        }
+    }
+
+    public class Token
+    {
+        public readonly TokenType type;
+
+        public int LineNumber;
+        public int CharNumber;
+
+
+        public Token(TokenType _type)
+        {
+            type = _type;
+        }
+
+        public enum TokenType
+        {
+            VariableDeclaration,
+            FunctionDeclaration,
+            Assignment,
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division,
+            Not,
+            And,
+            Or,
+            ArrayAccess,
+            Parameter,
+            Call,
+            String,
+            Int,
+            Double,
+            Bool
+
+        }
+    }
+}
