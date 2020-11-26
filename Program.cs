e64ee954f78f7e41fc0f4dfcf26fc31c5675c746
tylcos <tylcos@users.noreply.github.com>
2020-11-26 04:04:23 -0500

    Add TypeTable for performance and parse comments and string literals

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace TSL
 {
     internal class Program
     {
-        static readonly string PROGRAMPATH = @"\Example Files\in.txt";
-        static readonly string LEXEMEPATH  = @"\Example Files\out1.txt";
-        static readonly string TOKENSPATH  = @"\Example Files\out2.txt";
+        const string PROGRAMPATH = @"\Example Files\in.txt";
+        const string LEXEMEPATH  = @"\Example Files\out1.txt";
+        const string TOKENSPATH  = @"\Example Files\out2.txt";
 
 
 
         public static void Main()
         {
-            string program = File.ReadAllText(PROGRAMPATH);
+            string exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string program = File.ReadAllText(exePath + PROGRAMPATH);
 
             List<Lexeme> lexemes = new Lexer(program).GetLexemes();
-            IToken[] tokens = new Evaluator(lexemes).GetTokens();
+            //IToken[] tokens = new Evaluator(lexemes).GetTokens();
 
-            File.WriteAllLines(LEXEMEPATH, lexemes.Select(l => l.Type.ToString().PadRight(15) + l.Text));
+            File.WriteAllLines(exePath + LEXEMEPATH, lexemes.Select(l => l.Type.ToString().PadRight(15) + l.Text.Replace("\n", "\\n")));
             //File.WriteAllLines(TOKENSPATH, );
         }
     }
